fcc3020fb478d36ef3320649144ce8bdba1a399b
Néstor Coppi <nestorcoppi@gmail.com>
2018-10-29 11:38:19 -0300

    tangatang

--- wispro-install.sh
+++ wispro-install.sh
@@ -5,11 +5,11 @@
 
 alpine_version="v3.7"
 alpine_mirror="dl-3.alpinelinux.org"
-wispro_version="0.2.0"
+wispro_version="0.2.3"
 wispro_dir="/usr/src/app"
 wispro_binary="/usr/local/bin/wispro"
 wispro_binary_url=https://raw.githubusercontent.com/sequre/wispro_installer/master/wispro
-BMU_NGINX_VERSION="1.2"
+BMU_NGINX_VERSION="1.3"
 BMU_DNSMASQ_VERSION="1.3"
 FREERADIUS_VERSION="1.2"
 BMU_POSTGRESQL_VERSION="1.0"
